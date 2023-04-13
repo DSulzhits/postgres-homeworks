dd0fcff4d5479117e8929e315d1f854869162622
DSulzhits <dsulzhits@gmail.com>
2023-04-13 15:06:08 +0300

    Внесены исправления в соответствии с замечаниями

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -10,16 +10,16 @@ FROM orders
 ORDER BY ship_country, ship_city;
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-SELECT (SUM(shipped_date - order_date) / COUNT(*)) as days_average
+SELECT (AVG(shipped_date - order_date)) as days_average
 FROM orders
 WHERE ship_country = 'Germany';
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price)
 FROM products
-WHERE discontinued != 1;
+WHERE discontinued <> 1;
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price)
 FROM products
-WHERE discontinued != 1 and units_in_stock >= 20;
+WHERE discontinued <> 1 and units_in_stock >= 20;
